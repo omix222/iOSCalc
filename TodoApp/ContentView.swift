e6c7c63713b151ccc972fa0245a65fba79d41632
Hiromi Takahashi <hiromix222@gmail.com>
2026-01-24 17:18:36 +0900

    modiry message

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -7,7 +7,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             TodoListView()
-                .navigationTitle("TODO")
+                .navigationTitle("シンプルTODOアプリ")
                 .toolbar {
                     ToolbarItem(placement: .primaryAction) {
                         Button {
